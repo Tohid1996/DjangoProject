08f92f2b1ed7bf5f96b22f47146359c0da439a2d
Tohid1996 <63141398+Tohid1996@users.noreply.github.com>
2022-01-21 21:00:28 +0330

    Delete Projects/instagram directory

--- Projects/instagram/post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'post'
-urlpatterns = [
-    path('hello/', views.index, name='hello'),
-    path('posts_list/', views.post_list, name='posts_list'),
-]
